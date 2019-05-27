b5a0b64299e5e0120fe84d2ae28bbc46a0fb6c20
theapkk1 <thea.plenus.kjeldahl.kristensen@post.au.dk>
2019-05-27 10:51:33 +0200

    Add project files.

---
+++ Calculator.Unittest/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Calculatoreksamenslaesning.UnitTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        private Calculator _uut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Calculator();
+        }
+
+        [TestCase(2,5,7)]
+        [TestCase(-2,-6,-8)]
+        public void Addatob_returnresult(double a, double b, double result)
+        {
+            Assert.That(_uut.Add(a, b), Is.EqualTo(result));
+        }
+
+        [TestCase(5,4,1)]
+        [TestCase(-6,-2,-4)]
+        public void Subtractafromb_returnresult(double a, double b, double result)
+        {
+            Assert.That(_uut.Subtract(a,b), Is.EqualTo(result));
+        }
+
+        [TestCase(5,2,10)]
+        [TestCase(-2,3,-6)]
+        public void Multiplyatob_returnresult(double a, double b, double result)
+        {
+            Assert.That(_uut.Multiply(a,b), Is.EqualTo(result));
+        }
+
+        [TestCase(2,2,4)]
+        [TestCase(3,3,27)]
+        public void Power_returnresult(double x, double exp, double result)
+        {
+            Assert.That(_uut.Power(x,exp),Is.EqualTo(result));
+        }
+    }
+}
---
+++ Calculator_eksamenslæsning/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculatoreksamenslaesning
+{
+    public class Calculator
+    {
+        public double a { get; private set; }
+        public double b { get; private set; }
+        public double x { get; private set; }
+        public double exp { get; private set;}
+
+        public double Add(double a, double b)
+        {
+            double sumA = a + b;
+            return sumA;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            double sumS = a - b;
+            return sumS; 
+        }
+
+        public double Multiply(double a, double b)
+        {
+            double sumM = a * b;
+            return sumM;
+        }
+
+        public double Power(double x, double exp)
+        {
+            double xx = Math.Pow(x, exp);
+            return xx; 
+        }
+
+    }
+}
